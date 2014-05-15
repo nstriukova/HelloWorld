11c7b65f67cd0d14c6ffaaaefe46c45d317696a3
Nataliia <nataliia.s@mail.ru>
2014-05-15 18:52:47 +0300

    Commit for user2 story2

--- src/MyHelloWorld/HelloWorld.java
+++ src/MyHelloWorld/HelloWorld.java
@@ -3,6 +3,6 @@ package MyHelloWorld;
 /** Homework #1 */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, user2 branch world!");
+        System.out.println("Hello, user2 story2 world!");
     }
 }
